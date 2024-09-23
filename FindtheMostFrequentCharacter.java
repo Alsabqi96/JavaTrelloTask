03b5d0751196d6cf082f18cf5e024319623289b1
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2024-09-23 10:55:59 +0400

    Add files via upload
    This is code withe expleneation

--- FindtheMostFrequentCharacter.java
+++ FindtheMostFrequentCharacter.java
@@ -1,19 +1,23 @@
-public class FindtheMostFrequentCharacter{
+public class FindtheMostFrequentCharacter {
     public static void main(String[] args) {
-
+        // Test the method with the example input
         String str = "success";
         char result = mostFrequentChar(str);
         System.out.println("The most frequent character is: '" + result + "'");
     }
 
+    // Method to find the most frequent character in the string
     public static char mostFrequentChar(String str) {
+        // Array to store the frequency of characters (for ASCII values)
+        int[] frequency = new int[256]; // Assumes input contains only ASCII characters
 
-        int[] frequency = new int[256];
+        // Loop through the string to count the frequency of each character
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            frequency[c]++;
+            frequency[c]++; // Increment the frequency count for this character
         }
-        
+
+        // Find the character with the highest frequency
         int maxFrequency = 0;
         char mostFrequent = ' ';
 
@@ -28,6 +32,4 @@ public class FindtheMostFrequentCharacter{
         return mostFrequent;
     }
 }
-
-
 
