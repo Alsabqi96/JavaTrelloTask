f50e90efa6a5bfd490a8efa7624adad953a0a714
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2024-10-25 17:38:26 +0400

    Add files via upload

--- MapAB4.java
+++ MapAB4.java
@@ -23,12 +23,14 @@ public class MapAB4 {
     }
 
     public static Map<String, String> mapAB4(Map<String, String> map) {
-        if (map.containsKey("a")) {
-            map.put("c", map.get("a"));
-        } else if (map.containsKey("b")) {
-            map.put("c", map.get("b"));
+        if (map.containsKey("a") && map.containsKey("b")) {
+            if (map.get("a").length() != map.get("b").length()) {
+                map.put("c", map.get("a").length() > map.get("b").length() ? map.get("a") : map.get("b"));
+            } else {
+                map.put("a", "");
+                map.put("b", "");
+            }
         }
         return map;
     }
 }
-
