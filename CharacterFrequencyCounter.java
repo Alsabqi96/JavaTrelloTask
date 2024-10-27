2248b30e56c31c391c54177ae20025080b47ff0d
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2024-10-27 08:51:06 +0400

    Add files via upload
    This is the updated to the code

--- CharacterFrequencyCounter.java
+++ CharacterFrequencyCounter.java
@@ -1,31 +1,33 @@
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class CharacterFrequencyCounter {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a string: ");
+
+
         String input = scanner.nextLine().toLowerCase();
+        Map<Character, Integer> vowelMap = new TreeMap<>();
+        Map<Character, Integer> consonantMap = new TreeMap<>();
 
-        Map<Character, Integer> vowelMap = new HashMap<>();
-        Map<Character, Integer> consonantMap = new HashMap<>();
+
         String vowels = "aeiou";
-
         for (char ch : input.toCharArray()) {
             if (Character.isAlphabetic(ch)) {
                 if (vowels.indexOf(ch) >= 0) {
                     vowelMap.put(ch, vowelMap.getOrDefault(ch, 0) + 1);
-                } else {
+                }
+                else {
                     consonantMap.put(ch, consonantMap.getOrDefault(ch, 0) + 1);
                 }
             }
         }
-
         System.out.println("Vowel Frequencies: " + vowelMap);
         System.out.println("Consonant Frequencies: " + consonantMap);
 
         scanner.close();
     }
 }
-
