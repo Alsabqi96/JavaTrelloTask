6fee57b8ac260987428a05bada283267a0e74246
Abdullmajeed Al-Sabqi <134193752+Alsabqi96@users.noreply.github.com>
2024-10-27 08:29:37 +0400

    Add files via upload
    This is the last updated to the code

--- BunnyEars.java
+++ BunnyEars.java
@@ -1,19 +1,15 @@
 public class BunnyEars {
     public static void main(String[] args) {
-        BunnyEars be = new BunnyEars();
-        System.out.println("bunnyEars2(0) → " + be.bunnyEars2(0));
-        System.out.println("bunnyEars2(1) → " + be.bunnyEars2(1));
-        System.out.println("bunnyEars2(2) → " + be.bunnyEars2(2));
-
+        System.out.println("bunnyEars2(0) → " + bunnyEars2(0));
+        System.out.println("bunnyEars2(1) → " + bunnyEars2(1));
+        System.out.println("bunnyEars2(2) → " + bunnyEars2(2));
+        System.out.println("bunnyEars2(3) → " + bunnyEars2(3));
+        System.out.println("bunnyEars2(4) → " + bunnyEars2(4));
     }
-    
-    public int bunnyEars2(int bunnies) {
+
+    public static int bunnyEars2(int bunnies) {
         if (bunnies == 0) {
             return 0;
-        } else if (bunnies == 1) {
-            return 2;
-        } else if (bunnies == 2) {
-            return 5;
         } else if (bunnies % 2 != 0) {
             return bunnyEars2(bunnies - 1) + 2;
         } else {
@@ -21,6 +17,7 @@ public class BunnyEars {
         }
     }
 }
+
 
 
 
